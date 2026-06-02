f317b12a3cc025d4187232b43072fa6e2e59edb5
Andrew Zhou <andrewzhou135@gmail.com>
2026-06-02 23:22:57 +0800

    fix: add responsive mobile app navigation

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -6,7 +6,7 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-1 min-h-0">
+    <div className="flex flex-1 min-h-0 flex-col md:flex-row">
       <Sidebar />
       <div className="flex min-w-0 flex-1 flex-col min-h-0">
         {children}
